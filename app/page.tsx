0d8de1f3e7e80bb97d27aa50cc26a803d8f849bf
dolvin17 <huertas876@gmail.com>
2025-04-01 22:40:38 +0200

    adding all dependencies

--- app/page.tsx
+++ app/page.tsx
@@ -5,10 +5,9 @@ export default function Home() {
     <main className="px-4 py-12 max-w-xl mx-auto text-center grid place-items-center">
       <h1 className="text-3xl font-bold">Web3 Template by Radish</h1>
       <p className="mt-2 text-gray-600">
-        This is a template for building web3 applications using Next.js, viem,
-        wagmi and RainbowKit.{" "}
+      This is gonna be updated soon. Stay tuned!
         <strong className="font-semibold">✨ Includes:</strong> shadcn/ui
-        integration, and example hooks.
+        integration, and example hooks. Check out the <code>Example</code>{" "}
       </p>
       <Example className="mt-12" />
     </main>
