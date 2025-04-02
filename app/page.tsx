c2a9c154e34d29ac915e8c79f31925f2f5bd63c2
dolvin17 <huertas876@gmail.com>
2025-04-02 21:28:27 +0200

    adding fonts, web3 provider and scripts

--- app/page.tsx
+++ app/page.tsx
@@ -1,15 +1,8 @@
+import SectionHome from "@/components/SectionHome"
 import Example from "./Example"
 
 export default function Home() {
   return (
-    <main className="px-4 py-12 max-w-xl mx-auto text-center grid place-items-center">
-      <h1 className="text-3xl font-bold">Web3 Template by Radish</h1>
-      <p className="mt-2 text-gray-600">
-      This is gonna be updated soon. Stay tuned!
-        <strong className="font-semibold">✨ Includes:</strong> shadcn/ui
-        integration, and example hooks. Check out the <code>Example</code>{" "}
-      </p>
-      <Example className="mt-12" />
-    </main>
+	<SectionHome />
   )
 }
