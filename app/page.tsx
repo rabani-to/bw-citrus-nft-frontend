da38982f3ce98f390acf9651ec603067410107ba
dolvin17 <huertas876@gmail.com>
2025-04-03 23:31:18 +0200

    feat: connecting footer component

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,12 @@
 import SectionHome from "@/components/SectionHome"
+import SectionFAQ from "@/components/SectionFAQ"
 import Example from "./Example"
 
 export default function Home() {
   return (
+	<>
 	<SectionHome />
+	<SectionFAQ />
+	</>
   )
 }
