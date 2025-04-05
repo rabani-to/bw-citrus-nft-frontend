0f5110bfceab0248b4c27243927cfd58ac81153c
dolvin17 <huertas876@gmail.com>
2025-04-05 13:03:09 +0200

    feat: connectiong section origins

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 import SectionHome from "@/components/SectionHome"
 import SectionWhyCartel from "@/components/SectionWhyCartel"
+import SectionOrigins from "@/components/SectionOrigins"
 import SectionFAQ from "@/components/SectionFAQ"
 import Example from "./Example"
 
@@ -8,6 +9,7 @@ export default function Home() {
 	<>
 	<SectionHome />
 	<SectionWhyCartel />
+	<SectionOrigins />
 	<SectionFAQ />
 	</>
   )
