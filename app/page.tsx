77e45a7d1ff97f5fbd11caf30c0d841d97fdd07e
dolvin17 <huertas876@gmail.com>
2025-04-06 23:56:41 +0200

    feat:connecting SectionCommitment comp

--- app/page.tsx
+++ app/page.tsx
@@ -1,4 +1,5 @@
 import SectionHome from "@/components/SectionHome"
+import SectionCommitment from "@/components/SectionCommitment"
 import SectionWhyCartel from "@/components/SectionWhyCartel"
 import SectionOrigins from "@/components/SectionOrigins"
 import SectionFAQ from "@/components/SectionFAQ"
@@ -8,6 +9,7 @@ export default function Home() {
   return (
 	<>
 	<SectionHome />
+	<SectionCommitment />
 	<SectionWhyCartel />
 	<SectionOrigins />
 	<SectionFAQ />
